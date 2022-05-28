c65482a9731deaa4cb5c4ff5dabd89dc7a7adbd9
LayneHansen <layne.d.hansen@gmail.com>
2022-05-27 22:24:23 -0400

    added bootstrap, navbar, fixed react router bugs

--- hansen-react-portfolio/src/App.jsx
+++ hansen-react-portfolio/src/App.jsx
@@ -1,25 +1,38 @@
 import './App.css';
-import { Navbar } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Routes, Link, Redirect } from 'react-router-dom';
-import Homepage from './Homepage';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
 import Projects from './Projects';
 import Contact from './Contact';
 import Profile from './Profile'; 
 
 function App() {
   return (
-    <div className="App">
-      <h1>new portfolio</h1>
-
-    <Router>
+ <Router>
+      {/* <nav>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/contact">Contact Me</Link>
+      </nav> */}
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">ldh</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link to="/">Home</Nav.Link>
+          <Nav.Link to="/projects">Projects</Nav.Link>
+          <Nav.Link to="/profile">Profile</Nav.Link>
+          <Nav.Link to="/contact">Contact Me</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
       <Routes>
-        <Route path="/" component={Homepage} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/profile" component={Profile} />
+        <Route path="/" element={< Home />} />
+        <Route path="/projects" element={< Projects />} />
+        <Route path="/contact" element={< Contact />} />
+        <Route path="/profile" element={< Profile />} />
       </Routes>
     </Router>
-    </div>
   );
 }
 
--- hansen-react-portfolio/src/Home.jsx
+++ hansen-react-portfolio/src/Home.jsx
@@ -1,4 +1,4 @@
-function Homepage() {
+function Home() {
     return (
       <div className="App">
         <h1>homepage</h1>
@@ -7,4 +7,4 @@ function Homepage() {
     );
   }
   
-  export default Homepage;
+  export default Home;
