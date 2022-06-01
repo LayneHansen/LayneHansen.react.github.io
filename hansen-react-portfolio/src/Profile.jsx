8f3440df7dd91e02bb12e5d6c080ac8b784372ac
LayneHansen <layne.d.hansen@gmail.com>
2022-05-31 22:16:03 -0400

    updated profile page with image and text

--- hansen-react-portfolio/src/Profile.jsx
+++ hansen-react-portfolio/src/Profile.jsx
@@ -1,22 +1,43 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import profileImage from "../src/assets/LayneHorBW.jpg";
+import profileImage from "../src/assets/LayneColor.jpg";
+import './Profile.css';
 
 function Profile() {
   return (
     <Container>
-      <div className="App">
         <h1>profile</h1>
-      </div>
-      <Row>
-        <Col md={3}></Col>
-        <Col md={6}>
+      <Row className="row1-container">
+        <Col md={1}></Col>
+        <Col md={9} id="image-container">
+          <Row>
+            <Col>  
           <div>
-          <img src={profileImage} 
-          width="800px" />
+          <img id="profile-image"
+          src={profileImage} 
+          width="450px" 
+          alt="Layne Hansen portrait"
+          />
           </div>
+            </Col>
+            <Col>
+            <h1 id="name-header">Layne D. Hansen</h1>
+            <p class="profile-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              Architecto ea aperiam modi eum repellat voluptatem quasi, 
+              sed minus repellendus voluptas maxime minima commodi laborum 
+              rerum omnis illo aspernatur totam? Dignissimos.
+            </p>
+            <p class="profile-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              Architecto ea aperiam modi eum repellat voluptatem quasi, 
+              sed minus repellendus voluptas maxime minima commodi laborum 
+              rerum omnis illo aspernatur totam? Dignissimos.
+            </p>
+            </Col>
+          </Row>
         </Col>
-        <Col md={3}></Col>
+        <Col md={2}></Col>
       </Row>
 
     </Container>
