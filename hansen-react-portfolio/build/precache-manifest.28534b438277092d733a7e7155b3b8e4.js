c13e09d125afb99fc312923912eb68added18795
LayneHansen <layne.d.hansen@gmail.com>
2022-06-11 19:07:32 -0400

    added files to gh pages branch

---
+++ hansen-react-portfolio/build/precache-manifest.28534b438277092d733a7e7155b3b8e4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebbe7c4940bb016d73a6",
+    "url": "/static/css/main.c1764614.chunk.css"
+  },
+  {
+    "revision": "ebbe7c4940bb016d73a6",
+    "url": "/static/js/main.ebbe7c49.chunk.js"
+  },
+  {
+    "revision": "feee65e6ac648474e9a5",
+    "url": "/static/js/1.feee65e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a2eeef2002d31e09aba30d1c64a8b16d",
+    "url": "/static/media/workDeskImage5.a2eeef20.jpg"
+  },
+  {
+    "revision": "80cfae12dcd3c8bb1306b0791e4c9145",
+    "url": "/static/media/SureVoteLogo 2.80cfae12.png"
+  },
+  {
+    "revision": "fb81f690443ffbf772fa683857bb458e",
+    "url": "/static/media/RememberThemBanner.fb81f690.png"
+  },
+  {
+    "revision": "5bd2a363908af414c3c3995a3ef98728",
+    "url": "/static/media/StreamGuideLogo.5bd2a363.png"
+  },
+  {
+    "revision": "d16a9d4cb337b5f45299d68d2df3050f",
+    "url": "/static/media/PasswordGenerator.d16a9d4c.png"
+  },
+  {
+    "revision": "34c68c57b8cedbf66c2ea14becfc3ff8",
+    "url": "/static/media/linkedinlogo.34c68c57.png"
+  },
+  {
+    "revision": "09a3d6171a6b7f644da9f0b5926c6a89",
+    "url": "/static/media/githubwoodlogo.09a3d617.png"
+  },
+  {
+    "revision": "8fe068161d751e63033e00ab0ae7dbf1",
+    "url": "/static/media/resumelogo.8fe06816.png"
+  },
+  {
+    "revision": "36c8efd33d61049de1f185a1184d38aa",
+    "url": "/static/media/LayneColor.36c8efd3.jpg"
+  },
+  {
+    "revision": "cab18d552ebc78f66c36cbb6e3feb7ba",
+    "url": "/static/media/htmllogo.cab18d55.png"
+  },
+  {
+    "revision": "7b3fc6e9915a05a041e44d2f3389ad02",
+    "url": "/static/media/csslogo.7b3fc6e9.png"
+  },
+  {
+    "revision": "7da6d3eaa16ecfe9b8799f5350d12000",
+    "url": "/static/media/jslogo.7da6d3ea.png"
+  },
+  {
+    "revision": "599a1939de3fb8842f97e0419c5c61f0",
+    "url": "/static/media/reactlogo.599a1939.png"
+  },
+  {
+    "revision": "4195cf989fac0128a89669f40a1e3496",
+    "url": "/static/media/bootstraplogo.4195cf98.png"
+  },
+  {
+    "revision": "86f4ca760ce7c101edb2f991d1ea93ca",
+    "url": "/static/media/nodelogo.86f4ca76.png"
+  },
+  {
+    "revision": "f696f8207571a82c0850c70d534faf63",
+    "url": "/static/media/mongodblogo.f696f820.png"
+  },
+  {
+    "revision": "1a350423b8ca9640930797207d6d457a",
+    "url": "/static/media/sqllogo.1a350423.png"
+  },
+  {
+    "revision": "1091d3fcc7a80ac471a64a60f3d944a8",
+    "url": "/static/media/mernlogo.1091d3fc.png"
+  },
+  {
+    "revision": "e3702f05b3868bb7e073a29cfb281dd5",
+    "url": "/static/media/workDeskImage4.e3702f05.jpg"
+  },
+  {
+    "revision": "315e94d93d855c43738e183e50de0831",
+    "url": "/index.html"
+  }
+];
